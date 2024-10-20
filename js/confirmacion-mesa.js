8639fd5a2ba4eecbb2d8e1325ac7e61ea3db5d46
KenUp06 <kev.arias06@gmail.com>
2024-10-20 14:18:15 -0500

    minicambio

--- js/confirmacion-mesa.js
+++ js/confirmacion-mesa.js
@@ -6,7 +6,6 @@ document.getElementById('mesaForm').addEventListener('submit', function (e) {
     const ubicacion = document.getElementById('ubicacion').value;
 
     if (numMesa && capacidad && ubicacion) {
-        // Aquí puedes hacer una llamada a tu backend para agregar la mesa a la base de datos
         console.log(`Mesa agregada: Número ${numMesa}, Capacidad: ${capacidad}, Ubicación: ${ubicacion}`);
         alert("¡Mesa agregada correctamente!");
     } else {
